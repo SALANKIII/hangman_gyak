a96a8cc02db8afd7bd29a8930fcdcf56955b96e1
diak <diak@mesterlevel.lo>
2023-10-24 13:54:36 +0200

    2023.10.24

---
+++ lib/views/game_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hangman/controllers/game_view_controller.dart';
+
+class GameView extends StatelessWidget {
+  GameViewController controller = Get.put(GameViewController());
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<GameViewController>(
+      init: controller,
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Hangman"),
+          ),
+          body: Column(
+            children: [
+              Image.asset("images/0.png"),
+              Text(controller.randomSzoveg),
+              Text(controller.csillagosSzoveg),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      LetterButton(letter:"A", controller: controller),
+                      LetterButton(letter:"Á", controller: controller),
+                      LetterButton(letter:"B", controller: controller),
+                      LetterButton(letter:"C", controller: controller),
+                      LetterButton(letter:"D", controller: controller),
+                      LetterButton(letter:"E", controller: controller),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class LetterButton extends StatelessWidget {
+  const LetterButton({
+    required this.letter,
+    required this.controller,
+  });
+
+  final GameViewController controller;
+  final String letter;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton.filled(
+      padding: EdgeInsets.zero,
+      child: Text("A"),
+      onPressed: (!controller.aMegadottBetuVoltEmar("A"))
+        ? null
+        : ()=>controller.tippHozzaadasa("A"),
+    );
+  }
+}
